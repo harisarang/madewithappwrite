0b8fc3b0d394871b935314eafd7b0330ca4c8c0e
harisarang <hari@luxecraft.org>
2021-10-26 17:38:49 +0000

    add: appwrite functions

--- pages/submit.js
+++ pages/submit.js
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
 import ReactMarkdown from "react-markdown";
+import { GoMarkGithub } from "react-icons/go";
+import { BiLink } from "react-icons/bi";
+import Image from "next/image";
 
 export default function Submit() {
   const { register, handleSubmit, reset, watch, formState } = useForm({
@@ -21,7 +24,7 @@ export default function Submit() {
   return (
     <div className="flex flex-col min-h-screen bg-appPrimary pt-44">
       <h1 className="text-4xl text-center font-bold font-mono text-appwhite mb-10">
-        Submit a Project
+        Project Submission
       </h1>
       <form onSubmit={handleSubmit(updatePost)}>
         {preview && (
@@ -42,8 +45,63 @@ export default function Submit() {
                   );
                 })}
             </div>
-            <div className="rounded-lg w-2/3 m-3 p-10 outline-none bg-appSecondary text-appwhite shadow-sm">
-              <ReactMarkdown>{watch("content")}</ReactMarkdown>
+            <Image
+              src="https://eiitsgowqlbvulpsadlu.supabase.in/storage/v1/object/sign/images/logo.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJpbWFnZXMvbG9nby5wbmciLCJpYXQiOjE2MzM4ODg0MTIsImV4cCI6MTk0OTI0ODQxMn0.84dDS-HB6bScz36SnzHnVrn9cxVMgJlhh7onctx4Wfo"
+              width={500}
+              height={500}
+            />
+            <div className="flex justify-center">
+              <div className="rounded-lg w-1/2 m-3 p-10 outline-none text-appwhite shadow-sm">
+                <ReactMarkdown>
+                  {"# Description\n" + watch("content")}
+                </ReactMarkdown>
+              </div>
+              <div className="flex bg-appPrimary w-1/3 justify-center">
+                <div className="flex flex-col justify-center bg-appSecondary mt-20 h-1/4 rounded-3xl">
+                  <div className="text-appwhite mx-2 my-1 px-5 py-2">
+                    <a href={watch("github")}>
+                      <div className="flex items-center">
+                        <div className="mr-2">
+                          <GoMarkGithub />
+                        </div>
+                        <div>
+                          {watch("github")
+                            .replace("http://github.com/", "")
+                            .replace("https://github.com/", "")}
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                  <div className="text-appwhite m-2 px-5 py-2">
+                    <a href={watch("url")}>
+                      <div className="flex items-center">
+                        <div className="mr-2">
+                          <BiLink />
+                        </div>
+                        <div>
+                          {watch("url")
+                            .replace("https://", "")
+                            .replace("http://", "")}
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                  <div className="text-appwhite m-2 px-5 py-2">
+                    <p>Appwrite Features</p>
+                    <ul className="px-1">
+                      {watch("appwrite")
+                        .split(",")
+                        .map((appwrite) => {
+                          return (
+                            <li className="text-appwhite mx-2 p-1 px-3 rounded-full">
+                              {appwrite}
+                            </li>
+                          );
+                        })}
+                    </ul>
+                  </div>
+                </div>
+              </div>
             </div>
             <div className="w-2/3 flex flex-row">
               <button
@@ -136,7 +194,7 @@ export default function Submit() {
             id="github"
             placeholder="https://github.com/appwrite/appwrite"
             {...register("github", {
-              required: { value: true, message: "url is required" },
+              required: { value: true, message: "Github URL is required" },
             })}
           />
           <div className="w-2/3 m-1 flex flex-col items-start justify-start font-semibold">
@@ -149,12 +207,31 @@ export default function Submit() {
             rows="10"
             name="content"
             id="desc"
+            placeholder="Description about your project"
             {...register("content", {
               maxLength: { value: 2000, message: "Content is too long" },
               minLength: { value: 10, message: "Content is too small" },
               required: { value: true, message: "Content is required" },
             })}
           ></textarea>
+          <div className="w-2/3 m-1 flex flex-col items-start justify-start font-semibold">
+            <label for="appwrite" className="text-textSecondary text-bold">
+              Appwrite features (Comma separated)
+            </label>
+          </div>
+          <input
+            type="text"
+            className="w-2/3 mb-3 mx-3 px-5 py-3 rounded-lg outline-none bg-appSecondary text-appwhite shadow-sm"
+            name="appwrite"
+            id="appwrite"
+            placeholder="Functions, Database, Users, ..."
+            {...register("appwrite", {
+              required: {
+                value: true,
+                message: "Appwrite features is required",
+              },
+            })}
+          />
           {errors.content && (
             <p className=" text-appwhite">{errors.content.message}</p>
           )}
@@ -169,7 +246,13 @@ export default function Submit() {
             <button
               type="button"
               className=" bg-appwhite text-appPrimary m-3 p-3 w-1/2 rounded-lg shadow-sm"
-              onClick={() => setPreview(!preview)}
+              onClick={() => {
+                if (watch("title")) {
+                  setPreview(!preview);
+                } else {
+                  toast.error("Fill the form before previewing !");
+                }
+              }}
             >
               {preview ? "Edit" : "Preview"}
             </button>

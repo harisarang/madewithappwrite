66e133792837b95dceeee7f744051313f0f0f275
harisarang <hari@luxecraft.org>
2021-10-29 04:37:34 +0000

    update: next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["eiitsgowqlbvulpsadlu.supabase.in", "appwrite.harisaran.live"],
+    domains: ["eiitsgowqlbvulpsadlu.supabase.in", "appwrite.harisaran.live", "appwrite1.harisaran.live"],
   },
 };
